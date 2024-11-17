1af488dbb0843818b289940bb8e381df445a82dc
sopo <sofiko.imnaishvili@gmail.com>
2024-11-17 22:30:26 +0400

    add homepage layout and cards

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -12,20 +12,23 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { ModeToggle } from "../mode-toggle";
 import { Button } from "./button";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 export default function Header() {
   const { t, i18n } = useTranslation();
   const handleLanguageChange = (language: string) => {
     i18n.changeLanguage(language);
   };
+  const {lang} = useParams()
   return (
     <div className="border-b">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link to={`/${lang}`}>
         <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
           {t("blog")}
         </h2>
-        <NavigationMenu className="hidden md:flex space-x-4">
+        </Link>
+        <NavigationMenu className="hidden lg:flex space-x-4">
           <NavigationMenuList className="flex items-center px-4 py-2 space-x-4">
             <NavigationMenuItem className=" text-gray-900 dark:text-white">
               <Link to="/">{t("home")}</Link>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,72 @@
-export default function Home(){
-    return(
-        <p>home</p>
-    )
+import { useTranslation } from "react-i18next";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { badgeVariants } from "@/components/ui/badge"
+import { Link } from "react-router-dom";
+export default function Home() {
+    const { t } = useTranslation();
+  return (
+  <div className="px-4 flex-grow">
+    <div className="container mx-auto flex flex-col md:flex-row gap-8">
+    <section className="md:w-2/3 space-y-8 flex flex-col">
+    <Link to="">
+    <Card>
+      <CardHeader>
+      <img src="https://g-zwkebgiacpe.vusercontent.net/placeholder.svg?height=200&width=400" className="rounded-xl object-cover w-full h-[200px]"/>
+        <CardTitle>{t("homePage.cards.firstCard.title")}</CardTitle>
+        <CardDescription>
+            <p><Link to="" className="hover:underline">{t("homePage.cards.firstCard.author")}</Link> • {t("homePage.cards.firstCard.date")} • {t("homePage.cards.firstCard.timeToRead")}</p>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{t("homePage.cards.firstCard.content")}</p>
+      </CardContent>
+      <CardFooter className="flex flex-wrap gap-2">
+      <Link to="" className={badgeVariants({ variant: "secondary" })}>{t("homePage.cards.firstCard.badges.blockchain")}</Link>
+      <Link to="" className={badgeVariants({ variant: "secondary" })}>{t("homePage.cards.firstCard.badges.technology")}</Link>
+      <Link to="" className={badgeVariants({ variant: "secondary" })}>{t("homePage.cards.firstCard.badges.future")}</Link>
+      </CardFooter>
+    </Card>
+    </Link>
+    <Card>
+      <CardHeader>
+      <img src="https://g-zwkebgiacpe.vusercontent.net/placeholder.svg?height=200&width=400" className="rounded-xl object-cover w-full h-[200px]"/>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+    </section>
+  
+    <aside className="md:w-1/3 space-y-8">
+    <Card>
+      <CardHeader>
+        <CardTitle className="font-semibold text-md">Popular Tags</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-wrap gap-2">
+      <Link to="" className={badgeVariants({ variant: "default" })}>Badge</Link>
+      <Link to="" className={badgeVariants({ variant: "default" })}>Badge</Link>
+      <Link to="" className={badgeVariants({ variant: "default" })}>Badge</Link>
+      <Link to="" className={badgeVariants({ variant: "default" })}>Badjhkhkjhkhkhkhge</Link>
+      <Link to="" className={badgeVariants({ variant: "default" })}>Badge</Link>
+      <Link to="" className={badgeVariants({ variant: "default" })}>Badge</Link>
+      </CardContent>
+    </Card>
+    </aside>
+    </div>
+  </div>
+
+
+  );
 }
