34cd3ed65e68acc8479a611106533cbd6cce3df8
sopo <sofiko.imnaishvili@gmail.com>
2024-11-17 11:11:29 +0400

    add header translations

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -12,11 +12,9 @@ import {
   } from "@/components/ui/dropdown-menu"
 import { ModeToggle } from "../mode-toggle";
 import { Button } from "./button";
-import { Link, NavLink, useParams } from "react-router-dom";
+import { Link, NavLink,} from "react-router-dom";
 import { useTranslation } from "react-i18next";
 export default function Header() {
-    const { lang } = useParams();
-    const to = lang === 'en' ? '/ka' : '/en';
     const {t, i18n} = useTranslation()
     const handleLanguageChange = (language: string) => {
         i18n.changeLanguage(language);
@@ -30,17 +28,17 @@ export default function Header() {
         <NavigationMenuList className="flex items-center px-4 py-2 space-x-4">
           <NavigationMenuItem className=" text-gray-900 dark:text-white">
             <Link to="/">
-            home
+            {t("home")}
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem className=" text-gray-900 dark:text-white">
             <Link to="write">
-            Write
+            {t("write")}
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem className=" text-gray-900 dark:text-white">
             <Link to="about">
-            About
+            {t("about")}
             </Link>
           </NavigationMenuItem>
         </NavigationMenuList>
@@ -48,19 +46,19 @@ export default function Header() {
     <div className="flex items-center px-4 py-2 space-x-4">
       <Link to="sign-up">
         <Button className="bg-purple-500 hover:bg-purple-700 text-purple-50 rounded">
-          Sign up
+        {t("signIn")}
         </Button>
       </Link>
       <DropdownMenu >
   <DropdownMenuTrigger className=" text-gray-900 dark:text-white">Language</DropdownMenuTrigger>
   <DropdownMenuContent className=" bg-white dark:bg-gray-800">
     <DropdownMenuSeparator />
-    <NavLink to={to}>
+    <NavLink to={"/en"}>
     <DropdownMenuItem className=" text-gray-900 dark:text-white"
     onClick={() => handleLanguageChange("en")}
     >English</DropdownMenuItem>
     </NavLink>
-    <NavLink to={to}>
+    <NavLink to={"/ka"}>
     <DropdownMenuItem className=" text-gray-900 dark:text-white"
     onClick={() => handleLanguageChange("ka")}
     >ქართული</DropdownMenuItem>
