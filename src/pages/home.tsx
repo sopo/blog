f86ad918d89a5f7df4517ecef6bea6457b15517f
sopo <sofiko.imnaishvili@gmail.com>
2024-11-18 10:18:47 +0400

    add language detector

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -51,22 +51,41 @@ export default function Home() {
               </CardFooter>
             </Card>
           </Link>
-          <Card>
-            <CardHeader>
-              <img
-                src="https://g-zwkebgiacpe.vusercontent.net/placeholder.svg?height=200&width=400"
-                className="rounded-xl object-cover w-full h-[200px]"
-              />
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
+
+          <Link to="">
+            <Card>
+              <CardHeader>
+                <img
+                  src="https://g-zwkebgiacpe.vusercontent.net/placeholder.svg?height=200&width=400"
+                  className="mb-2 rounded-xl object-cover w-full h-[200px]"
+                />
+                <CardTitle>{t("homePage.cards.firstCard.title")}</CardTitle>
+                <CardDescription>
+                  <p>
+                    <Link to="" className="hover:underline">
+                      {t("homePage.cards.firstCard.author")}
+                    </Link>{" "}
+                    • {t("homePage.cards.firstCard.date")} •{" "}
+                    {t("homePage.cards.firstCard.timeToRead")}
+                  </p>
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>{t("homePage.cards.firstCard.content")}</p>
+              </CardContent>
+              <CardFooter className="flex flex-wrap gap-2">
+                <Link to="" className={badgeVariants({ variant: "secondary" })}>
+                  {t("homePage.cards.firstCard.badges.blockchain")}
+                </Link>
+                <Link to="" className={badgeVariants({ variant: "secondary" })}>
+                  {t("homePage.cards.firstCard.badges.technology")}
+                </Link>
+                <Link to="" className={badgeVariants({ variant: "secondary" })}>
+                  {t("homePage.cards.firstCard.badges.future")}
+                </Link>
+              </CardFooter>
+            </Card>
+          </Link>
         </section>
 
         <aside className="md:w-1/3 space-y-8">
--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import ka from './locales/ka/ka.json'
 import en from './locales/en/en.json'
+import LanguageDetector from 'i18next-browser-languagedetector';
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
@@ -13,15 +14,23 @@ const resources = {
     translation: ka
   }
 };
-
+const detectionOptions = {
+  order: ["path"],
+  lookUpFromPathIndex: 0,
+  lookUpFromSubdomainIndex: 0,
+  htmlTag: document.documentElement
+}
+const languageDetector = new LanguageDetector()
 i18n
+  .use(languageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+   detection: detectionOptions,
     resources,
-    lng: "ka", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    // lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
-    fallbackLng: "en",
+    // fallbackLng: "en",
     interpolation: {
       escapeValue: false // react already safes from xss
     }
