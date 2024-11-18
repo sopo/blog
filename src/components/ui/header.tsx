44527642dfa437ba49ea675a43ae8b0105732d20
sopo <sofiko.imnaishvili@gmail.com>
2024-11-18 10:50:26 +0400

    added search and language icons

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -14,19 +14,21 @@ import { ModeToggle } from "../mode-toggle";
 import { Button } from "./button";
 import { Link, NavLink, useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { Earth, Search } from "lucide-react";
+
 export default function Header() {
   const { t, i18n } = useTranslation();
   const handleLanguageChange = (language: string) => {
     i18n.changeLanguage(language);
   };
-  const {lang} = useParams()
+  const { lang } = useParams();
   return (
-    <div className="border-b">
+    <div className="border-b border-gray-200 dark:border-gray-700">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link to={`/${lang}`}>
-        <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
-          {t("blog")}
-        </h2>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+            {t("blog")}
+          </h2>
         </Link>
         <NavigationMenu className="hidden lg:flex space-x-4">
           <NavigationMenuList className="flex items-center px-4 py-2 space-x-4">
@@ -42,12 +44,24 @@ export default function Header() {
           </NavigationMenuList>
         </NavigationMenu>
         <div className="flex items-center space-x-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger className=" text-gray-900 dark:text-white">
+              <Search className=" hover:bg-slate-50 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-gray-800 dark:text-white text" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className=" bg-white dark:bg-gray-800"></DropdownMenuContent>
+          </DropdownMenu>
           <Link to="sign-in">
             <Button variant={"default"}>{t("signIn")}</Button>
           </Link>
           <DropdownMenu>
             <DropdownMenuTrigger className=" text-gray-900 dark:text-white">
-              Language
+              <Button
+                variant="outline"
+                size="icon"
+                className="rounded border-0 shadow hover:bg-slate-50"
+              >
+                <Earth className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-gray-800 dark:text-white text" />
+              </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className=" bg-white dark:bg-gray-800">
               <DropdownMenuSeparator />
--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -1,39 +1,39 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import ka from './locales/ka/ka.json'
-import en from './locales/en/en.json'
-import LanguageDetector from 'i18next-browser-languagedetector';
+import ka from "./locales/ka/ka.json";
+import en from "./locales/en/en.json";
+import LanguageDetector from "i18next-browser-languagedetector";
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
-    translation: en
+    translation: en,
   },
   ka: {
-    translation: ka
-  }
+    translation: ka,
+  },
 };
 const detectionOptions = {
   order: ["path"],
   lookUpFromPathIndex: 0,
   lookUpFromSubdomainIndex: 0,
-  htmlTag: document.documentElement
-}
-const languageDetector = new LanguageDetector()
+  htmlTag: document.documentElement,
+};
+const languageDetector = new LanguageDetector();
 i18n
   .use(languageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-   detection: detectionOptions,
+    detection: detectionOptions,
     resources,
     // lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
     // fallbackLng: "en",
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
-  export default i18n;
+export default i18n;
