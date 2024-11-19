6edb03e9e534a0766329737ea243c844d8c90745
sopo <sofiko.imnaishvili@gmail.com>
2024-11-19 23:22:03 +0400

    add tabs

--- src/pages/author/author.tsx
+++ src/pages/author/author.tsx
@@ -1,10 +1,12 @@
 import ScreenLg from "@/components/containers/page-containers/screen-lg"
 import AuthorInfoSection from "./author-info-section"
+import TabsSection from "./tabs-section"
 
 const Author:React.FC = () => {
     return(
        <ScreenLg>
             <AuthorInfoSection />
+            <TabsSection />
         </ScreenLg>
     )
 }
