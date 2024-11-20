d2b499041aa1c7ad96e8b64bec0fed38095e44a5
sopo <sofiko.imnaishvili@gmail.com>
2024-11-20 16:46:37 +0400

    fix Root to load data on first render

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,14 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui-blocks/footer";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
 export default function Root() {
+  const { lang } = useParams();
+  const { i18n } = useTranslation();
+  useEffect(() => {
+    if (lang) {
+      i18n.changeLanguage(lang);
+    }
+  }, [lang, i18n]);
 
   return (
     <div className="bg-color-surface">
       <Header />
-     <Outlet />
-     <Footer />
-
+      <Outlet />
+      <Footer />
     </div>
   );
 }
