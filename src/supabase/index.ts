aaa640c617273250b6fc0bc60b7d45e48945c796
Sopiko Imnaishvili <sopho.imnaishvili@gmail.com>
2024-11-21 10:08:52 +0400

    add supabase typescript

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import { Database } from './database.types'
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
+export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
+// supabase.from("")
+
