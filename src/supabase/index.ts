5eef4d24c934e18815afd0b246e8474cd762251b
sopo <sofiko.imnaishvili@gmail.com>
2024-11-21 10:12:20 +0400

    remove comment

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -3,5 +3,5 @@ import { Database } from './database.types'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
-// supabase.from("")
+
 
