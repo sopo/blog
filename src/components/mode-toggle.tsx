6c6a35a4e7556b0de557e48929e559b470d19f6d
sopo <sofiko.imnaishvili@gmail.com>
2024-11-22 21:04:59 +0400

    fix moon icon

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -6,6 +6,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useTheme } from "@/components/theme-provider";
+import { Button } from "./ui/button";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
@@ -13,11 +14,11 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <div className="p-2 rounded-full cursor-pointer">
+      <Button variant="outline" size="icon" >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-onSurfaceVariant" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-onSurfaceVariant" />
           <span className="sr-only">Toggle theme</span>
-        </div>
+        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
