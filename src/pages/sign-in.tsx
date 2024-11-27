f11453edb45a36a05de5a22f598b77e215c52095
Sopiko Imnaishvili <sopho.imnaishvili@gmail.com>
2024-11-27 11:57:35 +0400

    refactor sign in page to use useForm

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -7,9 +7,9 @@ import LabeledInputContainer from "@/components/containers/form-element-containe
 import FormContainer from "@/components/containers/form-element-containers/form-container";
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../supabase/auth";
-import { useState } from "react";
 import TextCenterSmallBlock from "@/components/ui-blocks/text-blocks/text-center-small-block";
 import AuthSection from "@/components/containers/section-containers/auth-section";
+import { useForm } from "react-hook-form";
 import {
   Card,
   CardContent,
@@ -17,14 +17,20 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
-
+type FormFields = {
+  email: string,
+  password: string
+}
 export default function SignIn() {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormFields>({
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  })
   const { t } = useTranslation();
   const { lang } = useParams();
-  const [loginData, setLoginData] = useState({ email: "", password: "" });
-  const [errorMessage, setErrorMessage] = useState("")
-  const navigate=useNavigate()
+  const navigate = useNavigate()
   const { mutate: handleLogin } = useMutation({
     mutationKey: ["login"],
     mutationFn: login,
@@ -33,72 +39,65 @@ export default function SignIn() {
     },
     onError: (error) => {
       console.error("Login failed", error);
-      setErrorMessage(error.message)
     },
   });
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (loginData.email && loginData.password) {
-      handleLogin(loginData);
+
+  const email = register("email", { required: t("logIn.errors.emptyEmailError") });
+  const password = register("password", {
+    required: t("logIn.errors.emptyPasswordError"), minLength: {
+      value: 4,
+      message: t("logIn.errors.insufficientPasswordError")
     }
-  };
+  });
   return (
     <AuthSection>
       <Card>
         <CardHeader>
           <CardTitle>
-            <TextCenterSmallBlock title={`${t("logIn.title")}`} description={`${t("logIn.description")}`}/>
+            <TextCenterSmallBlock title={`${t("logIn.title")}`} description={`${t("logIn.description")}`} />
           </CardTitle>
         </CardHeader>
         <CardContent>
-        <FormContainer onSubmit={handleSubmit}>
-          <LabeledInputContainer>
-            <Label>{t("logIn.email")}</Label>
-            <Input
-              value={loginData.email}
-              onChange={(e) => {
-                setLoginData({
-                  email: e.target.value,
-                  password: loginData.password,
-                });
-              }}
-              type="email"
-              id="email"
-              placeholder="john@example.com"
-            />
-          </LabeledInputContainer>
-          <LabeledInputContainer>
-            <Label>{t("logIn.password")}</Label>
-            <Input
-              value={loginData.password}
-              onChange={(e) => {
-                setLoginData({
-                  password: e.target.value,
-                  email: loginData.email,
-                });
-              }}
-              type="password"
-              id="password"
-            />
-          </LabeledInputContainer>
-          {errorMessage && <p className="paragraph-small-error">{errorMessage}</p> }
-          <Button
-            variant={"default"}
-            className="inline-flex items-center justify-center w-full"
-          >
-            {t("logIn.title")}
-          </Button>
-        </FormContainer>
+          <FormContainer onSubmit={handleSubmit((data) => {
+            handleLogin(data)
+          })}>
+            <LabeledInputContainer>
+              <Label>{t("logIn.email")}</Label>
+              <Input
+                {...email}
+                type="email"
+                id="email"
+                placeholder="john@example.com"
+              />
+              <p className="paragraph-small-error">{errors.email?.message}</p>
+            </LabeledInputContainer>
+            <LabeledInputContainer>
+              <Label>{t("logIn.password")}</Label>
+              <Input
+                {...password}
+                type="password"
+                id="password"
+              />
+              <p className="paragraph-small-error">{errors.password?.message}</p>
+            </LabeledInputContainer>
+
+            <Button
+              variant={"default"}
+              className="inline-flex items-center justify-center w-full"
+            >
+              {t("logIn.title")}
+            </Button>
+          </FormContainer>
         </CardContent>
         <CardFooter className="justify-between">
-        <Link
+          <Link
             to="forgot-password"
             className="text-sm text-primary hover:underline"
           >
             {t("logIn.forgotPassword")}
           </Link>
           <p className="paragraph-small-secondary">
-           {`${t("logIn.accountQuestion")} `}
+            {`${t("logIn.accountQuestion")} `}
             <Link
               to={`/${lang}/sign-up`}
               className="text-primary hover:underline"
@@ -111,6 +110,6 @@ export default function SignIn() {
 
 
 
-      </AuthSection>
+    </AuthSection>
   );
 }
