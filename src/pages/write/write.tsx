756e4f4a958b3ac44ae3c4526258ea5083f6bfd4
Sopiko Imnaishvili <sopho.imnaishvili@gmail.com>
2024-12-12 00:23:08 +0400

    refactor errors to display dynamic translations

--- src/pages/write/write.tsx
+++ src/pages/write/write.tsx
@@ -12,17 +12,11 @@ import { Label } from "@radix-ui/react-dropdown-menu";
 import { useAtomValue } from "jotai";
 import { useTranslation, Trans } from "react-i18next";
 
-; 
 const Write: React.FC = () => {
-    const user = useAtomValue(ProfileAtom);
+  const user = useAtomValue(ProfileAtom);
   const { t } = useTranslation();
-  const {
-    register,
-    handleSubmit,
-    errors,
-    handleImageChange,
-    onSubmit,
-  } = useArticleForm(user);
+  const { register, handleSubmit, errors, handleImageChange, onSubmit } =
+    useArticleForm(user);
 
   return (
     <AuthSection>
@@ -44,34 +38,58 @@ const Write: React.FC = () => {
           >
             <LabeledInputContainer>
               <Label>{t("writeArticle.fields.titleEn")}</Label>
-              <Input {...register("title_en", { required: t("writeArticle.fields.mandatoryError") })} type="text" id="title_en" placeholder="" />
-              <p className="paragraph-small-error">
-                {errors.title_en?.message}
-              </p>
+              <Input
+                {...register("title_en", { required: true })}
+                type="text"
+                id="title_en"
+                placeholder=""
+              />
+              {errors.title_en && (
+                <p className="paragraph-small-error">
+                  {t("writeArticle.fields.mandatoryError")}
+                </p>
+              )}
             </LabeledInputContainer>
 
             <LabeledInputContainer>
               <Label>{t("writeArticle.fields.titleKa")}</Label>
-              <Input {...register("title_ka", { required: t("writeArticle.fields.mandatoryError") })} type="text" id="title_ka" placeholder="" />
-              <p className="paragraph-small-error">
-                {errors.title_ka?.message}
-              </p>
+              <Input
+                {...register("title_ka", { required: true })}
+                type="text"
+                id="title_ka"
+                placeholder=""
+              />
+              {errors.title_ka && (
+                <p className="paragraph-small-error">
+                  {t("writeArticle.fields.mandatoryError")}
+                </p>
+              )}
             </LabeledInputContainer>
 
             <LabeledInputContainer>
               <Label>{t("writeArticle.fields.descriptionEn")}</Label>
-              <Textarea {...register("description_en", { required: t("writeArticle.fields.mandatoryError") })} id="description_en" />
-              <p className="paragraph-small-error">
-                {errors.description_en?.message}
-              </p>
+              <Textarea
+                {...register("description_en", { required: true })}
+                id="description_en"
+              />
+              {errors.description_en && (
+                <p className="paragraph-small-error">
+                  {t("writeArticle.fields.mandatoryError")}
+                </p>
+              )}
             </LabeledInputContainer>
 
             <LabeledInputContainer>
               <Label>{t("writeArticle.fields.descriptionKa")}</Label>
-              <Textarea {...register("description_ka", { required: t("writeArticle.fields.mandatoryError") })} id="description_ka" />
-              <p className="paragraph-small-error">
-                {errors.description_ka?.message}
-              </p>
+              <Textarea
+                {...register("description_ka", { required: true })}
+                id="description_ka"
+              />
+              {errors.description_ka && (
+                <p className="paragraph-small-error">
+                  {t("writeArticle.fields.mandatoryError")}
+                </p>
+              )}
             </LabeledInputContainer>
 
             <LabeledInputContainer>
